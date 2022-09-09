b275ae3190dad52e972ccc4519a1bcf36624d8e5
Prasad <banprasad21@gmail.com>
2022-09-09 07:02:37 +0530

    [Raghav] Add: code for string permutation

---
+++ DataStructureAndAlgorithms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataStructureAndAlgorithms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure & Algorithms");
+            Console.WriteLine("Select an option to proceed");
+            Console.WriteLine("1. String Permutation");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    StringPermutation stringPermutation = new StringPermutation();
+                    string str = "abc";
+                    int Output = str.Length;
+                    StringPermutation.Permute(str, 0, Output - 1);
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
---
+++ DataStructureAndAlgorithms/StringPermutation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithms
+{
+    class StringPermutation
+    {
+        public static void Permute(String str, int startingIndex, int endIndex)
+        {
+            if (startingIndex == endIndex)
+                Console.WriteLine(str);
+            else
+            {
+                for (int var_i = startingIndex; var_i <= endIndex; var_i++)
+                {
+                    str = Swap(str, startingIndex, var_i);
+                    Permute(str, startingIndex + 1, endIndex);
+                    str = Swap(str, startingIndex, var_i);
+                }
+            }
+        }
+       public static string Swap(string inputForWord, int left, int var_i)
+        {
+            char temp;
+            char[] charArray = inputForWord.ToCharArray();
+            temp = charArray[left];
+            charArray[left] = charArray[var_i];
+            charArray[var_i] = temp;
+            string Output = new string(charArray);
+            return Output;
+        }
+    }
+}
